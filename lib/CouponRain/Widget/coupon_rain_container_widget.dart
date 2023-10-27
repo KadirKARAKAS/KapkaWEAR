256df12dfed8afbbf617015074eb07089bb5e229
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2023-10-27 15:52:49 +0300

    save

--- lib/CouponRain/Widget/coupon_rain_container_widget.dart
+++ lib/CouponRain/Widget/coupon_rain_container_widget.dart
@@ -6,11 +6,47 @@ class CouponRainContainerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: 100,
-      decoration: BoxDecoration(
-          color: Colors.orange, borderRadius: BorderRadius.circular(8)),
+    return Stack(
+      children: [
+        Container(
+          width: size.width,
+          height: 100,
+          decoration: BoxDecoration(
+              color: Colors.orange, borderRadius: BorderRadius.circular(8)),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 10, top: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Her gün ücretsiz",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 2),
+              Text(
+                "Kupon kodunuz almayı ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "unutmayın!",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: const EdgeInsets.only(right: 35, top: 8),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Image(
+              image: AssetImage("assets/WheelofFortune.png"),
+              width: 80,
+              height: 80,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
